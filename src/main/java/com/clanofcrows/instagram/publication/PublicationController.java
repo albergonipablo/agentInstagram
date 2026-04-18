5a84751334003f12e92da1f892d4b9fc6aea8a43
Alber <albergonipablo@gmail.com>
2026-04-18 20:58:11 -0300

    Rename publication API routes

--- src/main/java/com/clanofcrows/instagram/publication/PublicationController.java
+++ src/main/java/com/clanofcrows/instagram/publication/PublicationController.java
@@ -10,22 +10,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/publications")
 @RequiredArgsConstructor
-public class TestPublicationController {
+public class PublicationController {
 
     private final PublishFeedPostUseCase publishFeedPostUseCase;
     private final PublishStoryUseCase publishStoryUseCase;
 
-    @PostMapping("/publish-feed")
-    public PublicationExecutionResult publishFeed(@RequestParam(required = false) String fileName) {
+    @PostMapping("/feed-posts")
+    public PublicationExecutionResult createFeedPostPublication(@RequestParam(required = false) String fileName) {
         return fileName == null || fileName.isBlank()
                 ? publishFeedPostUseCase.publishNext()
                 : publishFeedPostUseCase.publish(fileName);
     }
 
-    @PostMapping("/publish-story")
-    public PublicationExecutionResult publishStory(@RequestParam(required = false) String fileName) {
+    @PostMapping("/stories")
+    public PublicationExecutionResult createStoryPublication(@RequestParam(required = false) String fileName) {
         return fileName == null || fileName.isBlank()
                 ? publishStoryUseCase.publishNext()
                 : publishStoryUseCase.publish(fileName);
